042bfea55420161787b9a5d1eb4bb880ea2fadbf
Nicolas Alvarado <naav972@gmail.com>
2024-08-07 21:48:47 -0500

    idea

--- java/src/buffer.java
+++ java/src/buffer.java
@@ -2,11 +2,31 @@ import java.util.ArrayList;
 
 public class buffer {
 
-  private ArrayList<int[]> space;
-  private int num_mess;
+  private ArrayList<int[]> in;
+  private ArrayList<Integer> out;
+  private int num_in;
+  private int num_out;
 
-  public buffer(int size, int max_mess_size) {
-    this.num_mess = 0;
-    this.space = new ArrayList<int[]>();
+  public buffer() {
+    this.num_in = 0;
+    this.num_out = 0;
+    this.in = new ArrayList<int[]>();
+    this.out = new ArrayList<Integer>();
+  }
+
+  public void addIn(int[] mess) {
+    synchronized(this) {
+      this.in.add(mess);
+      this.num_in++;
+      notifyAll();
+    }
+  }
+
+  public void addOut(int mess) {
+    synchronized(this) {
+      this.out.add(mess);
+      this.num_out++;
+      notifyAll();
+    }
   }
 }
